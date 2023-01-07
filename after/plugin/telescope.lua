f2b9e1694f53bafbee29524d603762ad16a54444
Jakob Sjudin <jakob.sjudin@gmail.com>
2023-01-07 18:10:29 +0100

    Add a few telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -12,7 +12,9 @@ require 'telescope'.setup {
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "(Telescope) [f]ind [f]iles" })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "(Telescope) [C-p] git files" })
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "(Telescope) [f]ind [w]ord" })
+vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = "(Telescope) [f]ind [b]ranch" })
+vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = "(Telescope) [t]elescope [k]eymaps" })
+vim.keymap.set('n', '<leader>tc', builtin.commands, { desc = "(Telescope) [t]elescope [c]ommands" })
