970581a30e4979bcda5588d20ea39082e6440410
Jakob Sjudin <jakob.sjudin@gmail.com>
2023-01-21 14:15:52 +0100

    Add keybinds for jumping between hunks

--- lua/jakob/remap.lua
+++ lua/jakob/remap.lua
@@ -3,9 +3,6 @@ vim.g.mapleader = " "
 -- Paste visual selection without overwriting register
 vim.keymap.set("v", "<leader>p", '"_dP')
 
--- Source rc file
-vim.keymap.set("n", "<C-s>", ":so ~/.config/nvim/init.lua<CR>")
-
 -- keep cursor centered on jump
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
