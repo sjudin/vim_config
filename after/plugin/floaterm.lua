8bbd90a94eda5b7e772a16711cb3be73f4b6a644
jakob <jakob@pop-os.localdomain>
2023-10-16 21:26:45 +0200

    Change floatterm window size

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,4 +1,6 @@
 vim.g.floaterm_autoclose = 2
+vim.g.floaterm_width = 0.85
+vim.g.floaterm_height = 0.85
 
 vim.keymap.set("n", "<leader>tt", vim.cmd.FloatermToggle, { desc = "(floaterm) [tt]oggle" })
 vim.keymap.set("t", "<leader>tt", vim.cmd.FloatermToggle, { desc = "(floaterm) [tt]oggle" })
