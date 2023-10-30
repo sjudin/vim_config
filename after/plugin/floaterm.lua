86cfae49105a147319492a58dec81a34056da35d
jakob <jakob@pop-os.localdomain>
2023-10-30 21:06:53 +0100

    Change floatterm binding

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -2,5 +2,4 @@ vim.g.floaterm_autoclose = 2
 vim.g.floaterm_width = 0.85
 vim.g.floaterm_height = 0.85
 
-vim.keymap.set("n", "<leader>tt", vim.cmd.FloatermToggle, { desc = "(floaterm) [tt]oggle" })
-vim.keymap.set("t", "<leader>tt", vim.cmd.FloatermToggle, { desc = "(floaterm) [tt]oggle" })
+vim.keymap.set({"n", "t"}, "<C-t>", vim.cmd.FloatermToggle, { desc = "(floaterm) [tt]oggle" })
