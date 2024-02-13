82e70b7f6f7f402c104bfa85f08c59b64a214415
Jakob Sjudin <jakob.sjudin@saferadar.se>
2024-02-13 14:29:15 +0100

    Add jk=esc keymap

--- lua/jakob/remap.lua
+++ lua/jakob/remap.lua
@@ -12,6 +12,7 @@ vim.keymap.set("i", ",", ",<c-g>u")
 vim.keymap.set("i", "?", "?<c-g>u")
 vim.keymap.set("i", ".", ".<c-g>u")
 vim.keymap.set("i", "!", "!<c-g>u")
+vim.keymap.set("i", "jk", "<Esc>")
 
 -- moving selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
