4a04573e49368e2c686a2211c39864137affae96
jakob <jakob@pop-os.localdomain>
2024-05-25 10:19:42 +0200

    Use neovim native comments (v0.10.0)

--- lua/jakob/init.lua
+++ lua/jakob/init.lua
@@ -86,3 +86,16 @@ autocmd({ 'BufNewFile', 'BufRead' },
             vim.cmd [[set filetype=groovy]]
         end
     })
+
+local nvim_version = vim.version()
+-- Fix neovims janky comments
+if (nvim_version.major >= 0 and nvim_version.minor >= 10) then
+    vim.api.nvim_create_autocmd({ 'FileType' }, {
+      desc = 'Force commentstring to include spaces',
+      -- group = ...,
+      callback = function(event)
+        local cs = vim.bo[event.buf].commentstring
+        vim.bo[event.buf].commentstring = cs:gsub('(%S)%%s', '%1 %%s'):gsub('%%s(%S)', '%%s %1')
+      end,
+    })
+end
