5eb5f21a57eb12d51f88261dded8777f9a0bbb2d
jakob <jakob@pop-os.localdomain>
2024-05-25 21:38:52 +0200

    Bump to harpoon2

--- lua/jakob/plugins/harpoon.lua
+++ lua/jakob/plugins/harpoon.lua
@@ -1,10 +1,13 @@
 return {
-    'ThePrimeagen/harpoon',
-    config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-    end,
+    config = function()
+        local harpoon = require('harpoon')
+        harpoon:setup()
+
+        vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    end
 }
