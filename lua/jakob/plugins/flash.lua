4b1e67c6c43da00e5d1cbddd85c73efbc40ec757
jakob <jakob@pop-os.localdomain>
2024-05-26 21:49:29 +0200

    Make flash feel a bit nicer

--- lua/jakob/plugins/flash.lua
+++ lua/jakob/plugins/flash.lua
@@ -3,6 +3,7 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+        { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
     },
 }
--- lua/jakob/set.lua
+++ lua/jakob/set.lua
@@ -26,6 +26,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
