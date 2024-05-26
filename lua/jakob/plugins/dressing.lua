ba7ed9a69bb304103c04ff585d8e7febaebffc84
jakob <jakob@pop-os.localdomain>
2024-05-26 21:50:08 +0200

    add dressing

---
+++ lua/jakob/plugins/dressing.lua
@@ -0,0 +1,10 @@
+return {
+    "stevearc/dressing.nvim",
+    opts = {
+        input = {
+            enabled = true,
+            start_in_insert = false,
+            relative = "editor",
+        }
+    },
+}
