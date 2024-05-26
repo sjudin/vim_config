02567cac13bdf8a8f336cf66a2517de3befaa3f6
jakob <jakob@pop-os.localdomain>
2024-05-26 12:11:29 +0200

    Use floating window for oil

--- lua/jakob/plugins/oil.lua
+++ lua/jakob/plugins/oil.lua
@@ -4,7 +4,13 @@ return {
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("oil").setup()
-        vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+        require("oil").setup(
+            {
+                keymaps = {
+                    ["q"] = "actions.close",
+                }
+            }
+        )
+        vim.keymap.set("n", "-", require("oil").open_float, { desc = "Open parent directory" })
     end
 }
