d795993d9607a46de663e66aa91ba68df4457a07
Jakob Sjudin <jakob.sjudin@gmail.com>
2024-05-27 20:29:51 +0200

    Switch harpoon keybinds

--- lua/jakob/plugins/harpoon.lua
+++ lua/jakob/plugins/harpoon.lua
@@ -7,7 +7,10 @@ return {
         local harpoon = require('harpoon')
         harpoon:setup()
 
-        vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set(
+            "n", "m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+            { desc = "Toggle harpoon menu" }
+        )
+        vim.keymap.set("n", "<leader>m", function() harpoon:list():add() end)
     end
 }
