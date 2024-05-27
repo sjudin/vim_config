625fa56b11e477845a56fb7f106c11d6155d13b6
jakob <jakob@pop-os.localdomain>
2024-05-27 16:28:42 +0200

    tweak some flash settings

--- lua/jakob/plugins/flash.lua
+++ lua/jakob/plugins/flash.lua
@@ -1,7 +1,15 @@
 return {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        -- Colemak labels
+        labels = "arstgmneioqwfpbjluyäzxcdvkh",
+        modes = {
+            char = {
+                enabled = false
+            }
+        }
+    },
     keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
         { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
