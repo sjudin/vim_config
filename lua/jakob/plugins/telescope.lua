e503a3b1f3105117521405f862b9a74af4c935b7
Jakob Sjudin <jakob.sjudin@gmail.com>
2024-11-26 09:22:50 +0100

    Add aerial

--- lua/jakob/plugins/telescope.lua
+++ lua/jakob/plugins/telescope.lua
@@ -1,10 +1,9 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim' 
-        },
-        config = function()
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim'
+    },
+    config = function()
         local actions = require('telescope.actions')
         local builtin = require('telescope.builtin')
 
@@ -26,15 +25,33 @@ return {
                         ["<C-a>"] = actions.smart_send_to_qflist + actions.open_qflist,
                     }
                 }
-            }
-            }
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "(Telescope) [f]ind [f]iles" })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "(Telescope) [C-p] git files" })
-vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "(Telescope) [f]ind [w]ord" })
-vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = "(Telescope) [f]ind [b]ranch" })
-vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "(Telescope) [f]ind [s]ymbol" })
-vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = "(Telescope) [t]elescope [k]eymaps" })
-vim.keymap.set('n', '<leader>tc', builtin.commands, { desc = "(Telescope) [t]elescope [c]ommands" })
-        end
-    }
+            },
+            extensions = {
+                aerial = {
+                    -- Set the width of the first two columns (the second
+                    -- is relevant only when show_columns is set to 'both')
+                    col1_width = 4,
+                    col2_width = 30,
+                    -- How to format the symbols
+                    format_symbol = function(symbol_path, filetype)
+                        if filetype == "json" or filetype == "yaml" then
+                            return table.concat(symbol_path, ".")
+                        else
+                            return symbol_path[#symbol_path]
+                        end
+                    end,
+                    -- Available modes: symbols, lines, both
+                    show_columns = "both",
+                },
+            },
+        }
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "(Telescope) [f]ind [f]iles" })
+        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "(Telescope) [C-p] git files" })
+        vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "(Telescope) [f]ind [w]ord" })
+        vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = "(Telescope) [f]ind [b]ranch" })
+        vim.keymap.set('n', '<leader>fs', require('telescope').extensions.aerial.aerial,
+            { desc = "(Telescope) [f]ind [s]ymbol" })
+        vim.keymap.set('n', '<leader>tk', builtin.keymaps, { desc = "(Telescope) [t]elescope [k]eymaps" })
+        vim.keymap.set('n', '<leader>tc', builtin.commands, { desc = "(Telescope) [t]elescope [c]ommands" })
+    end
 }
