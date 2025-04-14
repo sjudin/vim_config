157e62d4a37f71b07e26f02f95794f82d6f97b81
Jakob Sjudin <jakob.sjudin@saferadar.se>
2025-04-14 12:22:47 +0200

    update some keymap stuff

--- lua/jakob/lsp.lua
+++ lua/jakob/lsp.lua
@@ -16,23 +16,12 @@ vim.diagnostic.config({
     }
 })
 
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover,
-    { border = 'rounded' }
-)
-
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signature_help,
-    { border = 'rounded' }
-)
-
 local map = function(m, lhs, rhs, bufnr, desc)
     local key_opts = { buffer = bufnr, desc = desc }
     vim.keymap.set(m, lhs, rhs, key_opts)
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    desc = 'LSP actions',
     callback = function(args)
         -- local client = vim.lsp.get_client_by_id(args.data.client_id)
         -- print(string.format("Attaching %s to buffer %d", client.name, args.buf))
@@ -40,7 +29,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- LSP actions
         map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, args.buf, "(lsp) [c]ode [a]ction")
         map('n', '<leader>rn', vim.lsp.buf.rename, args.buf, "(lsp) [r]e[n]ame")
-        map('n', 'K', vim.lsp.buf.hover, args.buf, "(lsp) [K]hover doc")
+        map('n', 'K', function() vim.lsp.buf.hover { border = "rounded" } end, args.buf, "(lsp) [K]hover doc")
 
         map('n', 'gd', vim.lsp.buf.definition, args.buf, "(lsp) [g]o [d]efinition")
         map('n', 'gD', vim.lsp.buf.declaration, args.buf, "(lsp) [g]oto [D]eclaration")
@@ -51,9 +40,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         -- Diagnostics
         map('n', 'gl', vim.diagnostic.open_float, args.buf, "(lsp) [gl]ine diagnostics")
-        map('n', '<C-[>', vim.diagnostic.goto_prev, args.buf, "(lsp) [gk] diagnostic jump previous")
-        map('n', '<C-]>', vim.diagnostic.goto_next, args.buf, "(lsp) [gj] diagnostic jump next")
-        -- map('n', 'gi', vim.lsp.buf.implementation, args.buf, "(lsp) [g]oto [i]mplementation")
+        map('n', '<C-[>', function() vim.diagnostic.jump { count = -1, float = true } end, args.buf,
+            "(lsp) [gk] diagnostic jump previous")
+        map('n', '<C-]>', function() vim.diagnostic.jump { count = 1, float = true } end, args.buf,
+            "(lsp) [gj] diagnostic jump next")
 
         -- Formatting
         map('n', 'gm', vim.lsp.buf.format, args.buf, "(lsp) [g]o for[m]at")
