ec1515ddffe4abc045582abea2780cdbd95a63fc
jakob <jakob.sjudin@saferadar.se>
2025-12-19 20:58:45 +0100

    Show current CWD in oil winbar

--- lua/jakob/plugins/oil.lua
+++ lua/jakob/plugins/oil.lua
@@ -1,3 +1,15 @@
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+    local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+    local dir = require("oil").get_current_dir(bufnr)
+    if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+    else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+    end
+end
+
 return {
     'stevearc/oil.nvim',
     opts = {},
@@ -8,7 +20,10 @@ return {
             {
                 keymaps = {
                     ["q"] = "actions.close",
-                }
+                },
+                win_options = {
+                    winbar = "%!v:lua.get_oil_winbar()",
+                },
             }
         )
         vim.keymap.set("n", "-", require("oil").open_float, { desc = "Open parent directory" })
