4544620edc25bcb6cd3b95297969b86d175801d6
Jakob Sjudin <jakob.sjudin@gmail.com>
2026-02-20 21:41:51 +0100

    Drop dressing for snacks.input

--- lua/jakob/plugins/snacks.lua
+++ lua/jakob/plugins/snacks.lua
@@ -6,5 +6,29 @@ return {
     opts = {
         bigfile = { enabled = true },
         quickfile = { enabled = true },
+        input = {
+            enabled = true,
+
+            -- Window-specific configuration goes inside the 'win' table
+            win = {
+                -- Centers the floating window relative to the entire editor
+                relative = "editor",
+                position = "float",
+
+                -- Snacks.input natively enters insert mode when opened.
+                -- We use the 'on_buf' callback to intercept it and stop insert mode.
+                on_buf = function(_)
+                    vim.schedule(function()
+                        vim.cmd("stopinsert")
+                        vim.cmd("normal! ^")
+                    end)
+                end,
+            }
+        },
+        picker = {
+            enabled = true,
+            -- This explicitly tells Snacks to hijack vim.ui.select
+            ui_select = true,
+        }
     }
 }
