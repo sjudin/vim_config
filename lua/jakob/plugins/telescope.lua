826e5c9b04617831bb0352f719ed6fd0b8b9beec
Jakob Sjudin <jakob.sjudin@gmail.com>
2026-02-27 20:15:37 +0100

    Telescope tweaks

--- lua/jakob/plugins/telescope.lua
+++ lua/jakob/plugins/telescope.lua
@@ -53,12 +53,41 @@ return {
         },
     },
     keys = {
-        { "<leader>ff", builtin.find_files,                            desc = "(Telescope) [f]ind [f]iles" },
-        { "<leader>fw", builtin.live_grep,                             desc = "(Telescope) [f]ind [w]ord" },
-        { "<leader>fb", builtin.git_branches,                          desc = "(Telescope) [f]ind [b]ranch" },
-        { "<leader>fs", require('telescope').extensions.aerial.aerial, desc = "(Telescope) [f]ind [s]ymbol" },
-        { "<leader>tk", builtin.keymaps,                               desc = "(Telescope) [t]elescope [k]eymaps" },
-        { "<leader>tc", builtin.commands,                              desc = "(Telescope) [t]elescope [c]ommands" },
-        { "<C-p>",      builtin.git_files,                             desc = "(Telescope) [<C-p> Git files]" },
+        { "<leader>ff", builtin.find_files,   desc = "(Telescope) [f]ind [f]iles" },
+        { "<leader>fw", builtin.live_grep,    desc = "(Telescope) [f]ind [w]ord" },
+        { "<leader>fb", builtin.git_branches, desc = "(Telescope) [f]ind [b]ranch" },
+        {
+            "<leader>fs",
+            -- When confirming a search using aerial we want to make it so that the body
+            -- of the function is in the middle of the screen with the confirmed search
+            -- at the top
+            function()
+                require('telescope').extensions.aerial.aerial({
+                    ---@diagnostic disable-next-line: unused-local
+                    attach_mappings = function(prompt_bufnr, map)
+                        local select_and_offset = function(bufnr)
+                            actions.select_default(bufnr)
+
+                            local offset_rows = vim.o.scrolloff
+                            local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+
+                            local top_line = math.max(1, cursor_line - offset_rows)
+                            vim.fn.winrestview({ topline = top_line })
+                        end
+
+                        map('i', '<CR>', select_and_offset)
+                        map('n', '<CR>', select_and_offset)
+
+                        -- Return true to keep all other default Telescope mappings
+                        return true
+                    end
+                })
+            end,
+            desc = "(Telescope) [f]ind [s]ymbol"
+        },
+        { "<leader>tk", builtin.keymaps,   desc = "(Telescope) [t]elescope [k]eymaps" },
+        { "<leader>th", builtin.help_tags, desc = "(Telescope) [t]elescope [h]elptags" },
+        { "<leader>tc", builtin.commands,  desc = "(Telescope) [t]elescope [c]ommands" },
+        { "<C-p>",      builtin.git_files, desc = "(Telescope) [<C-p> Git files]" },
     },
 }
