8171e6803546e1bab853c332b0857ab537adceec
Jakob Sjudin <jakob.sjudin@gmail.com>
2026-02-27 19:56:53 +0100

    Change substitute/flash keybinding (welcome back x key)

--- lua/jakob/plugins/flash.lua
+++ lua/jakob/plugins/flash.lua
@@ -11,7 +11,7 @@ return {
         }
     },
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
-        { "r", mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+        { "<leader><leader>", mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+        { "r",                mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
     },
 }
--- lua/jakob/plugins/substitute.lua
+++ lua/jakob/plugins/substitute.lua
@@ -15,8 +15,8 @@ return {
         { "cxx", mode = "n",          desc = "Exchange line" },
         { "X",   mode = "x",          desc = "Exchange visual" },
         { "cxc", mode = "n",          desc = "Exchange cancel" },
-        { "x",   mode = { "n", "x" }, desc = "Substitute operator/visual" },
-        { "xx",  mode = "n",          desc = "Substitute line" },
+        { "s",   mode = { "n", "x" }, desc = "Substitute operator/visual" },
+        { "ss",  mode = "n",          desc = "Substitute line" },
         { "X",   mode = "n",          desc = "Substitute to eol" },
     },
 
@@ -34,9 +34,9 @@ return {
         vim.keymap.set("n", "cxc", exchange.cancel, { noremap = true, desc = "Exchange cancel" })
 
         -- Substitute mappings
-        vim.keymap.set("n", "x", substitute.operator, { noremap = true, desc = "Substitute" })
-        vim.keymap.set("n", "xx", substitute.line, { noremap = true, desc = "Substitute line" })
-        vim.keymap.set("n", "X", substitute.eol, { noremap = true, desc = "Substitute to eol" })
-        vim.keymap.set("x", "x", substitute.visual, { noremap = true, desc = "Substitute visual" })
+        vim.keymap.set("n", "s", substitute.operator, { noremap = true, desc = "Substitute" })
+        vim.keymap.set("n", "ss", substitute.line, { noremap = true, desc = "Substitute line" })
+        vim.keymap.set("n", "S", substitute.eol, { noremap = true, desc = "Substitute to eol" })
+        vim.keymap.set("x", "s", substitute.visual, { noremap = true, desc = "Substitute visual" })
     end,
 }
