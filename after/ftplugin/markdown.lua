4b325f77144fd9505237dfae54cd0a496856765b
Jakob Sjudin <jakob.sjudin@gmail.com>
2026-02-28 12:27:19 +0100

    Misc changes

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+-- Enable wrapping for markdown files
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
--- lua/jakob/set.lua
+++ lua/jakob/set.lua
@@ -1,11 +1,14 @@
-vim.opt.hidden = false
-
 vim.opt.guicursor = ""
--- vim.opt.iskeyword = vim.opt.iskeyword - "_"
 
 -- line numbering
 vim.opt.nu = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
+
+-- Split directions
+vim.opt.splitright = true
+-- vim.opt.splitbelow = true
+
+vim.opt.showmode = false
 
 -- spaces over tabs, use 4 space indent
 vim.opt.tabstop = 4
@@ -13,8 +16,8 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- no swapfile
 vim.opt.smartindent = true
+vim.opt.breakindent = true
 vim.opt.wrap = false
 
 -- no swapfile or backup
@@ -33,6 +36,10 @@ vim.opt.colorcolumn = "80"
 
 -- always keep 8 lines when scrolling
 vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Make it look good
 vim.opt.updatetime = 50
@@ -47,7 +54,7 @@ vim.opt.sessionoptions = vim.opt.sessionoptions - "buffers"
 
 vim.opt.visualbell = true
 
--- Hide statusline
+-- One global statusline
 vim.o.laststatus = 3
 
 -- show cursorcolumn
